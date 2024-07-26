e0599efb8ec8245fda928089823e670581cd2b7a
loademon <loademon@tutanota.com>
2024-07-26 18:07:29 +0300

    Refactor code for consistency and readability
    - Updated function definitions to use consistent formatting and indentation
    
    - Ensured all file operations use `"utf-8"` encoding
    - Added `ensure_ascii=False` to `write_json` for proper handling of non-ASCII characters

--- botasaurus/utils.py
+++ botasaurus/utils.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+
 def is_errors_instance(instances, error):
     for i in range(len(instances)):
         ins = instances[i]
@@ -8,37 +9,47 @@ def is_errors_instance(instances, error):
             return True, i
     return False, -1
 
+
 def relative_path(path, goback=0):
     levels = [".."] * (goback + -1)
     return os.path.abspath(os.path.join(os.getcwd(), *levels, path.strip()))
 
+
 def remove_nones(list):
     return [element for element in list if element is not None]
 
-def write_html( data, path,):
-    with open(path, 'w', encoding="utf-8") as fp:
+
+def write_html(
+    data,
+    path,
+):
+    with open(path, "w", encoding="utf-8") as fp:
         fp.write(data)
 
 
-def write_file( data, path,):
-    with open(path, 'w', encoding="utf-8") as fp:
+def write_file(
+    data,
+    path,
+):
+    with open(path, "w", encoding="utf-8") as fp:
         fp.write(data)
 
 
 def read_json(path):
-    with open(path, 'r', encoding="utf-8") as fp:
+    with open(path, "r", encoding="utf-8") as fp:
         data = json.load(fp)
         return data
 
+
 def read_file(path):
-    with open(path, 'r', encoding="utf-8") as fp:
+    with open(path, "r", encoding="utf-8") as fp:
         content = fp.read()
         return content
-        
-def write_json(data, path,  indent=4):
-    with open(path, 'w', encoding="utf-8") as fp:
-        json.dump(data, fp, indent=indent)
 
+
+def write_json(data, path, indent=4):
+    with open(path, "w", encoding="utf-8") as fp:
+        json.dump(data, fp, indent=indent, ensure_ascii=False)
 
 
 def uniquify_strings(strs):
